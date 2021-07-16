f595f97cec43aeb604cdbaea6d639efa47b9ea8f
techadd9 <techadd9@gmail.com>
2021-07-16 14:25:02 +0900

    Updates

---
+++ precache-manifest.dcf2abdab4208ff3e297aa2cea5485ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a22ef15378a83e0f9227da2d8ffeda5b",
+    "url": "/react_habit/index.html"
+  },
+  {
+    "revision": "e201e50cdbc7213080cc",
+    "url": "/react_habit/static/css/main.9b00fc92.chunk.css"
+  },
+  {
+    "revision": "386d9d3139988ee5a91b",
+    "url": "/react_habit/static/js/2.f295e262.chunk.js"
+  },
+  {
+    "revision": "64f54ce285484345cd5e3bd96b120148",
+    "url": "/react_habit/static/js/2.f295e262.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e201e50cdbc7213080cc",
+    "url": "/react_habit/static/js/main.bcfcf896.chunk.js"
+  },
+  {
+    "revision": "e23b6792b26a8d5bde87",
+    "url": "/react_habit/static/js/runtime-main.8e43ec5b.js"
+  }
+]);
